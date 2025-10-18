805ef38f5bdcbe4840d8d121c862433ccef5d5b1
Matvii Bosyi <bosyimatvii@gmail.com>
2025-10-18 15:50:31 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-pizza-sushi/sw.js', { scope: '/react-pizza-sushi/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
